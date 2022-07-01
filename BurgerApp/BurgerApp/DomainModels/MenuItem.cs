89f46590fb6c8b78242155a2061c027f13649ff0
AnitaNikolovska <63537596+AnitaNikolovska@users.noreply.github.com>
2022-07-01 11:40:38 +0200

    domain models

---
+++ BurgerApp/BurgerApp/DomainModels/MenuItem.cs
@@ -0,0 +1,20 @@
+﻿namespace BurgerApp.DomainModels
+{
+    public class MenuItem
+    {
+        public int Id { get; set; }
+        public Burger BurgerId { get; set; }
+        public Size SizeId { get; set; }
+        public decimal Price { get; set; }
+
+        public MenuItem() { }
+
+        public MenuItem(int id, Burger burgerId, Size sizeId, decimal price)
+        {
+            Id = id;
+            BurgerId = burgerId;
+            SizeId = sizeId;
+            Price = price;
+        }
+    }
+}
---
+++ BurgerApp/BurgerApp/DomainModels/OrderItem.cs
@@ -0,0 +1,20 @@
+﻿namespace BurgerApp.DomainModels
+{
+    public class OrderItem
+    {
+        public int Id { get; set; }
+        public MenuItem MenuItemId { get; set; }
+        public Order OrderId { get; set; }
+        public int Quantity { get; set; }
+
+        public OrderItem() { }
+
+        public OrderItem(int id, MenuItem menuItemId, Order orderId, int quantity)
+        {
+            Id = id;
+            MenuItemId = menuItemId;
+            OrderId = orderId;
+            Quantity = quantity;
+        }
+    }
+}
